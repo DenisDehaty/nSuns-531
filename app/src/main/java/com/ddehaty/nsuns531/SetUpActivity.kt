5bed24052bcbd6bb5aae2fb3d3abce838a227f43
DenisDehaty <denis.dehaty@protonmail.com>
2021-04-27 16:38:38 +0200

    added alertDialog

--- app/src/main/java/com/ddehaty/nsuns531/SetUpActivity.kt
+++ app/src/main/java/com/ddehaty/nsuns531/SetUpActivity.kt
@@ -2,9 +2,10 @@ package com.ddehaty.nsuns531
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.text.InputType
+import android.text.method.DigitsKeyListener
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 
 class SetUpActivity : AppCompatActivity() {
@@ -23,8 +24,49 @@ class SetUpActivity : AppCompatActivity() {
             val unitsCheckedRadioButtonId =
                 findViewById<RadioGroup>(R.id.unitsRadioButton).checkedRadioButtonId
             val units = findViewById<RadioButton>(unitsCheckedRadioButtonId)
+            showAlertDialog()
         }
 
+    }
+
+    fun createLayoutForDialog(): LinearLayout {
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+
+        val squatInput = EditText(this)
+        squatInput.hint = this.getString(R.string.squat)
+        squatInput.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        linearLayout.addView(squatInput)
+
+        val benchInput = EditText(this)
+        benchInput.hint = this.getString(R.string.bench)
+        benchInput.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        linearLayout.addView(benchInput)
+
+        val dlInput = EditText(this)
+        dlInput.hint = this.getString(R.string.deadlift)
+        dlInput.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        linearLayout.addView(dlInput)
+
+        val ohpInput = EditText(this)
+        ohpInput.hint = this.getString(R.string.ohp)
+        ohpInput.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        linearLayout.addView(ohpInput)
+
+        return linearLayout
+    }
+
+    fun showAlertDialog() {
+        val layout = createLayoutForDialog()
+        val alertDialogBuilder = AlertDialog.Builder(this)
+            .setTitle(R.string.set_personal_max_title).setMessage(R.string.set_personal_max_msg)
+            .setPositiveButton(R.string.confirm_button) { _, _ ->
+                TODO("Sprav prechod na dalsiu aktivitu")
+            }
+            .setNegativeButton(R.string.back_button) { dialog, _ ->
+                dialog.dismiss()
+            }.setView(layout)
+            .show()
     }
 
 }
