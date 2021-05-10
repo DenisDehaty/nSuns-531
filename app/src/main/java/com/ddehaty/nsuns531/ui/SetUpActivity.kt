378ddca8dc2593491d358d1d644a7ffb8aa9ecf8
DenisDehaty <denis.dehaty@protonmail.com>
2021-05-10 14:06:55 +0200

    bug fixes for alert_dialog.xml, amoled theme instead of dark

--- app/src/main/java/com/ddehaty/nsuns531/ui/SetUpActivity.kt
+++ app/src/main/java/com/ddehaty/nsuns531/ui/SetUpActivity.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.doAfterTextChanged
 import com.ddehaty.nsuns531.*
 import com.ddehaty.nsuns531.db.NsunsDatabase
 import com.ddehaty.nsuns531.db.WeightRepository
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -56,26 +56,39 @@ class SetUpActivity : AppCompatActivity() {
     private fun showAlertDialog() {
         val inflater = this.layoutInflater
         val layout = inflater.inflate(R.layout.alert_dialog, null)
-        AlertDialog.Builder(this)
+        val dialog = AlertDialog.Builder(this)
             .setTitle(R.string.set_personal_max_title).setMessage(R.string.set_personal_max_msg)
-            .setPositiveButton(R.string.confirm_button) { dialog, _ ->
-                val benchpressWeight = layout.findViewById<EditText>(R.id.benchpressEditText)
-                val deadliftWeigt = layout.findViewById<EditText>(R.id.deadliftEditText)
-                val ohpWeight = layout.findViewById<EditText>(R.id.ohpEditText)
-                val squatWeigt = layout.findViewById<EditText>(R.id.squatEditText)
-                saveWeights(
-                    Benchpress(weight = benchpressWeight.text.toString()),
-                    Deadlift(weight = deadliftWeigt.text.toString()),
-                    Ohp(weight = ohpWeight.text.toString()),
-                    Squat(weight = squatWeigt.text.toString())
-                )
-                dialog.dismiss()
-                // TODO("Sprav prechod na dalsiu aktivitu")
-            }
+            .setPositiveButton(R.string.confirm_button, null)
             .setNegativeButton(R.string.back_button) { dialog, _ ->
                 dialog.dismiss()
-            }.setView(layout)
+            }
+            .setView(layout)
             .show()
-    }
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.isEnabled = false
+        val benchpressWeight = layout.findViewById<EditText>(R.id.benchpressEditText)
+        val deadliftWeigt = layout.findViewById<EditText>(R.id.deadliftEditText)
+        val ohpWeight = layout.findViewById<EditText>(R.id.ohpEditText)
+        val squatWeigt = layout.findViewById<EditText>(R.id.squatEditText)
+        positiveButton.isEnabled = false
+        val weights = listOf(benchpressWeight, deadliftWeigt, ohpWeight, squatWeigt)
+        weights.forEach { weight ->
+            weight.doAfterTextChanged {
+                positiveButton.isEnabled =
+                    !(benchpressWeight.text.toString() == "" || deadliftWeigt.text.toString() == "" ||
+                            ohpWeight.text.toString() == "" || squatWeigt.text.toString() == "")
+            }
+        }
+        positiveButton.setOnClickListener {
+            saveWeights(
+                Benchpress(weight = benchpressWeight.text.toString()),
+                Deadlift(weight = deadliftWeigt.text.toString()),
+                Ohp(weight = ohpWeight.text.toString()),
+                Squat(weight = squatWeigt.text.toString())
+            )
+            dialog.dismiss()
+        }
 
+    }
 }
+
