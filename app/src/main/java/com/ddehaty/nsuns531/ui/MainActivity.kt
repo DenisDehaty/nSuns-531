8683f7205eee29c74aa5e0110c2fda49d36eb555
DenisDehaty <denis.dehaty@protonmail.com>
2021-05-30 00:31:39 +0200

    added navigation menu

--- app/src/main/java/com/ddehaty/nsuns531/ui/MainActivity.kt
+++ app/src/main/java/com/ddehaty/nsuns531/ui/MainActivity.kt
@@ -3,21 +3,61 @@ package com.ddehaty.nsuns531.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.ddehaty.nsuns531.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val preferences = getSharedPreferences("Preferences", MODE_PRIVATE)
 
-        if(preferences.getBoolean("firststart",true)){
+        if (preferences.getBoolean("firststart", true)) {
             val setUpActivity = Intent(this, SetUpActivity::class.java)
             startActivity(setUpActivity)
             finish()
         }
 
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val navView = findViewById<NavigationView>(R.id.navView)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> Toast.makeText(applicationContext, "home", Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(applicationContext,"settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 
--- app/src/main/java/com/ddehaty/nsuns531/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/ddehaty/nsuns531/ui/fragments/HomeFragment.kt
@@ -1,16 +1,21 @@
 package com.ddehaty.nsuns531.ui.fragments
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.ddehaty.nsuns531.adapters.ViewPagerAdapter
 import com.ddehaty.nsuns531.R
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,21 +32,20 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }.attach()
 
 
+}
 
-
-
-    }
-
-    private fun getTypeOfTraining() : Int {
-        val preferences = this.requireActivity().getSharedPreferences("Preferences", AppCompatActivity.MODE_PRIVATE)
+    private fun getTypeOfTraining(): Int {
+        val preferences = this.requireActivity()
+            .getSharedPreferences("Preferences", AppCompatActivity.MODE_PRIVATE)
         val fragmentActivity = this.activity
         var trainingType = 4
-        when (preferences?.getString("plan","4")) {
+        when (preferences?.getString("plan", "4")) {
             fragmentActivity?.getString(R.string.four_day_plan) -> trainingType = 1
             fragmentActivity?.getString(R.string.five_day_plan) -> trainingType = 2
             fragmentActivity?.getString(R.string.six_day_deadlift_plan) -> trainingType = 3
             fragmentActivity?.getString(R.string.six_day_squat_plan) -> trainingType = 4
-            else -> {}
+            else -> {
+            }
 
         }
         return trainingType
